bc7e58db12e3db4e7c0e42245d02cecec103eaab
Alessandro Cattabiani <alessandro.cattabiani@gmail.com>
2026-07-19 11:04:46 +0200

    feat: implement data collator and Trainer configuration for Qwen2-VL QLoRA

--- 06-equation-to-code/main.py
+++ 06-equation-to-code/main.py
@@ -2,9 +2,11 @@ from pathlib import Path
 from datasets import load_from_disk, load_dataset
 import torch
 from latex2sympy2_extended import latex2sympy
-from transformers import Qwen2VLForConditionalGeneration, AutoProcessor, BitsAndBytesConfig
+from transformers import Qwen2VLForConditionalGeneration, AutoProcessor, BitsAndBytesConfig, TrainingArguments, Trainer
 model_id = "Qwen/Qwen2-VL-2B-Instruct"
 import sympy as sp
+from peft import LoraConfig, TaskType
+from peft import get_peft_model
 
 
 def collate_fn(batch):
@@ -14,21 +16,43 @@ def collate_fn(batch):
     }
 
 
-def preprocess_batch(processor, data):
-    messages_batch = [
-        [
-            {
-                "role": "user",
-                "content": [
-                    {"type": "image", "image": img},
-                    {"type": "text", "text": "Convert this equation into Python code."},
-                ],
-            }
+def preprocess_batch(processor, data, is_train):
+    if is_train: 
+        messages_batch = [
+            [
+                {
+                    "role": "user",
+                    "content": [
+                        {"type": "image", "image": img},
+                        {"type": "text", "text": "Convert this equation into Python code."},
+                        
+                    ],
+                },
+                {
+                    "role": "assistant",
+                    "content": [
+                        {"type": "text", "text": ans},
+                    ]
+                }
+            ]
+            for img, ans in zip(data["images"], data["sympy_formulas"])
         ]
-        for img in data["images"]
-    ]
+    else:
+        messages_batch = [
+            [
+                {
+                    "role": "user",
+                    "content": [
+                        {"type": "image", "image": img},
+                        {"type": "text", "text": "Convert this equation into Python code."},
+                        
+                    ],
+                }
+            ]
+            for img, ans in zip(data["images"], data["sympy_formulas"])
+        ]
     texts = [
-        processor.apply_chat_template(m, tokenize=False, add_generation_prompt=True)
+        processor.apply_chat_template(m, tokenize=False, add_generation_prompt=(not is_train))
         for m in messages_batch
     ]
 
@@ -38,6 +62,27 @@ def preprocess_batch(processor, data):
         padding=True,
         return_tensors="pt"
     )
+    if is_train:
+        # 1. Start by cloning the input_ids
+        labels = inputs["input_ids"].clone()
+        
+        # 2. Iterate through each sequence in the batch
+        for i in range(labels.shape[0]):
+            seq = labels[i].tolist()
+            for idx in range(len(seq) -1):
+                if seq[idx] == 151644 and seq[idx+1] == 77091:
+                    labels[i, :idx+2] = -100
+                    break
+            # We want to find where the sequence [151644, 77091] (<|im_start|>assistant) is located in labels[i]
+            # Once found at index j, set labels[i, :j + 2] = -100
+            
+        # 3. Add the masked labels back to the inputs dictionary
+        labels[inputs["attention_mask"] == 0] = -100
+        inputs["labels"] = labels
+        
+
+
+
     return inputs
 
 def postproc_output_ids(processor, inputs, output_ids):
@@ -85,7 +130,7 @@ dataset = load_from_disk(str(processed_path))
 
 dataset = dataset.train_test_split(test_size=0.1, seed=42)
 train_data = dataset['train']
-val_data = dataset['test']
+eval_data = dataset['test']
 
 dataloader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
 
@@ -99,13 +144,62 @@ quantization_config = BitsAndBytesConfig(
     bnb_4bit_use_double_quant=True,
     bnb_4bit_quant_type="nf4"
 )
+
+
+peft_config = LoraConfig(task_type=TaskType.CAUSAL_LM, 
+                inference_mode=False, 
+                r=8, 
+                lora_alpha=32, 
+                lora_dropout=0.1,
+                target_modules=["q_proj", "k_proj", "v_proj", "o_proj"]
+                )
+
+
 # Load model and processor
 model = Qwen2VLForConditionalGeneration.from_pretrained(
     model_id,
     quantization_config=quantization_config,
     device_map="auto"
 )
+model = get_peft_model(model, peft_config)
+model.print_trainable_parameters()
+
+
+training_args = TrainingArguments(
+    output_dir="data/lora_finetuning",
+    learning_rate=2e-4,
+    per_device_train_batch_size=2,
+    gradient_accumulation_steps=16,
+    per_device_eval_batch_size=2,
+    optim="paged_adamw_8bit",
+    num_train_epochs=2,
+    weight_decay=0.01,
+    eval_strategy="epoch",
+    save_strategy="epoch",
+    load_best_model_at_end=True,
+)
 processor = AutoProcessor.from_pretrained(model_id)
+def data_collator(data):
+    return preprocess_batch(processor, collate_fn(data), is_train=True)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_data,
+    eval_dataset=eval_data,
+    processing_class=processor.tokenizer,
+    data_collator=data_collator,
+)
+
+trainer.train()
+
+
+exit()
+
+
+
+
+
 
 for data in dataloader:
     inputs = preprocess_batch(processor, data)
